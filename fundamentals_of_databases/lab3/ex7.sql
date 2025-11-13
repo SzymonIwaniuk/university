0a350e532920bccb52f144681c10eaa009baad5e
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-11-13 16:45:01 +0100

    fix merge conflitcs

---
+++ fundamentals_of_databases/lab3/ex7.sql
@@ -0,0 +1,9 @@
+SELECT s.ShipperID,
+        s.CompanyName,
+        SUM(o.Freight) AS TotalFreight
+FROM Shippers AS s  
+JOIN Orders AS o
+    ON s.ShipperID = o.ShipVia
+    WHERE o.ShippedDate >= '1996-01-01' AND o.ShippedDate < '1997-01-01'
+GROUP BY s.ShipperID, s.CompanyName
+ORDER BY TotalFreight DESC; 
