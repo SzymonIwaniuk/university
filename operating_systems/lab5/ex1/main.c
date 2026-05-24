1047adfe3d5f9adfef3780e129e41899d1ffebee
SzymonIwaniuk <szymon.iwaniuk@joinmoxie.com>
2026-05-24 14:47:33 +0200

    operating_systems: lab5 final

--- operating_systems/lab5/ex1/main.c
+++ operating_systems/lab5/ex1/main.c
@@ -11,26 +11,26 @@
 
 #define N 5
 #define M 5
-#define K 5
+#define K 64
 #define STR_LEN 10
 
 #define SHM_NAME "/myshm"
 #define SEM_DONE "/done"
-#define SEM_WRITE_ACCESS "/write_access"
-#define SEM_READ_MUTEX "/read_mutex"
+#define SEM_ORDER "/order"
+#define SEM_MUTEX "/mutex"
 #define SEM_FULL "/full_%d"
 #define SEM_WRITER "/writer_%d"
 #define CHARSET "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 typedef struct {
-  char buffer[K][STR_LEN + 1];
+  char buffer[(K / (STR_LEN + 1))][STR_LEN + 1];
   int in;
   int out;
   int reader_count;
 } SharedData;
 
 SharedData *shdata;
-sem_t *sem_done, *sem_write_access, *sem_read_mutex;
+sem_t *sem_done, *sem_order, *sem_mutex;
 sem_t *sem_full[M];
 sem_t *sem_writers[N];
 
@@ -49,21 +49,21 @@ void producer(int id) {
   while (1) {
     generate_string(str);
     sem_wait(sem_writers[id]);
-    sem_wait(sem_write_access);
+    sem_wait(sem_order);
 
     strcpy(shdata->buffer[shdata->in], str);
-    shdata->in = (shdata->in + 1) % K;
+    shdata->in = (shdata->in + 1) % (K / (STR_LEN + 1));
 
-    printf("[Producer %d] wrote: \"%s\"\n", id + 1, str);
+    printf("[Producer %d] wrote: %s\n", id + 1, str);
     fflush(stdout);
 
-    sem_post(sem_write_access);
+    sem_post(sem_order);
     for (int j = 0; j < M; j++)
       sem_post(sem_full[j]);
     for (int j = 0; j < M; j++)
       sem_wait(sem_done);
 
-    shdata->out = (shdata->out + 1) % K;
+    shdata->out = (shdata->out + 1) % (K / (STR_LEN + 1));
     sem_post(sem_writers[(id + 1) % N]);
   }
 }
@@ -71,31 +71,32 @@ void producer(int id) {
 void consumer(int id) {
   char line[64];
   char str[STR_LEN + 1];
+
   while (1) {
     sem_wait(sem_full[id]);
-    sem_wait(sem_read_mutex);
+    sem_wait(sem_mutex);
 
     shdata->reader_count++;
     if (shdata->reader_count == 1)
-      sem_wait(sem_write_access);
+      sem_wait(sem_order);
 
-    sem_post(sem_read_mutex);
+    sem_post(sem_mutex);
 
     strcpy(str, shdata->buffer[shdata->out]);
-    sem_wait(sem_read_mutex);
+    sem_wait(sem_mutex);
     shdata->reader_count--;
     if (shdata->reader_count == 0)
-      sem_post(sem_write_access);
-    sem_post(sem_read_mutex);
+      sem_post(sem_order);
+    sem_post(sem_mutex);
     sem_post(sem_done);
-    int pos = snprintf(line, sizeof line, "[Consumer %d] reading: \"", id + 1);
+    int pos = snprintf(line, sizeof line, "[Consumer %d] reading: ", id + 1);
 
     write(STDOUT_FILENO, line, pos);
     for (int i = 0; str[i] != '\0'; i++) {
       write(STDOUT_FILENO, &str[i], 1);
       usleep(300000);
     }
-    write(STDOUT_FILENO, "\"\n", 2);
+    write(STDOUT_FILENO, "\n", 1);
   }
 }
 
@@ -106,8 +107,8 @@ int main(void) {
   // clean last session
   shm_unlink(SHM_NAME);
   sem_unlink(SEM_DONE);
-  sem_unlink(SEM_WRITE_ACCESS);
-  sem_unlink(SEM_READ_MUTEX);
+  sem_unlink(SEM_ORDER);
+  sem_unlink(SEM_MUTEX);
 
   for (int j = 0; j < M; j++) {
     snprintf(name, sizeof name, SEM_FULL, j);
@@ -140,14 +141,14 @@ int main(void) {
     perror("done");
     exit(1);
   }
-  if ((sem_write_access = sem_open(SEM_WRITE_ACCESS, O_CREAT | O_EXCL, 0666,
-                                   1)) == SEM_FAILED) {
-    perror("write");
+  if ((sem_order = sem_open(SEM_ORDER, O_CREAT | O_EXCL, 0666, 1)) ==
+      SEM_FAILED) {
+    perror("order");
     exit(1);
   }
-  if ((sem_read_mutex = sem_open(SEM_READ_MUTEX, O_CREAT | O_EXCL, 0666, 1)) ==
+  if ((sem_mutex = sem_open(SEM_MUTEX, O_CREAT | O_EXCL, 0666, 1)) ==
       SEM_FAILED) {
-    perror("read_mutex");
+    perror("mutex");
     exit(1);
   }
   for (int j = 0; j < M; j++) {
@@ -200,8 +201,8 @@ int main(void) {
   // cleanup
   shm_unlink(SHM_NAME);
   sem_unlink(SEM_DONE);
-  sem_unlink(SEM_WRITE_ACCESS);
-  sem_unlink(SEM_READ_MUTEX);
+  sem_unlink(SEM_ORDER);
+  sem_unlink(SEM_MUTEX);
 
   for (int j = 0; j < M; j++) {
     snprintf(name, sizeof name, SEM_FULL, j);
---
+++ operating_systems/lab5/ex3/main.c
@@ -0,0 +1,278 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define N 5
+#define M 5
+#define K 128
+#define STR_LEN 10
+#define PRIORITY_CHANCE 30
+#define MANAGER_INTERVAL 5
+
+#define SHM_NAME "/myshm3"
+#define SEM_ANY "/any3"
+#define SEM_FULL_PRIORITY "/full_priority3"
+#define SEM_EMPTY_PRIORITY "/empty_priority3"
+#define SEM_MUTEX_PRIORITY "/mutex_priority3"
+#define SEM_FULL_NORMAL "/full_normal3"
+#define SEM_EMPTY_NORMAL "/empty_normal3"
+#define SEM_MUTEX_NORMAL "/mutex_normal3"
+#define SEM_ORDER "/order3_%d"
+#define CHARSET "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+typedef struct {
+  char priority_buf[((K / 2) / (STR_LEN + 1))][STR_LEN + 1];
+  int priority_in;
+  int priority_out;
+  char normal_buf[((K / 2) / (STR_LEN + 1))][STR_LEN + 1];
+  int normal_in;
+  int normal_out;
+} SharedData;
+
+SharedData *shdata;
+sem_t *sem_any;
+sem_t *sem_full_priority, *sem_empty_priority, *sem_mutex_priority;
+sem_t *sem_full_normal, *sem_empty_normal, *sem_mutex_normal;
+sem_t *sem_order[N];
+
+void generate_string(char *buf) {
+  for (int i = 0; i < STR_LEN; i++)
+    buf[i] = CHARSET[rand() % (int)(sizeof CHARSET - 1)];
+  buf[STR_LEN] = '\0';
+}
+
+void producer(int id) {
+  srand(time(NULL) + getpid());
+  char str[STR_LEN + 1];
+  while (1) {
+    generate_string(str);
+    sem_wait(sem_order[id]);
+
+    int is_priority = (rand() % 100) < PRIORITY_CHANCE;
+    if (is_priority) {
+      sem_wait(sem_empty_priority);
+      sem_wait(sem_mutex_priority);
+      strcpy(shdata->priority_buf[shdata->priority_in], str);
+      shdata->priority_in =
+          (shdata->priority_in + 1) % ((K / 2) / (STR_LEN + 1));
+      printf("[Producer %d] PRIORITY: %s\n", id + 1, str);
+      fflush(stdout);
+      sem_post(sem_mutex_priority);
+      sem_post(sem_full_priority);
+    } else {
+      sem_wait(sem_empty_normal);
+      sem_wait(sem_mutex_normal);
+      strcpy(shdata->normal_buf[shdata->normal_in], str);
+      shdata->normal_in = (shdata->normal_in + 1) % ((K / 2) / (STR_LEN + 1));
+      printf("[Producer %d] NORMAL: %s\n", id + 1, str);
+      fflush(stdout);
+      sem_post(sem_mutex_normal);
+      sem_post(sem_full_normal);
+    }
+
+    sem_post(sem_any);
+    sem_post(sem_order[(id + 1) % N]);
+  }
+}
+
+void consumer(int id) {
+  char line[64];
+  char str[STR_LEN + 1];
+  const char *queue_name;
+  while (1) {
+    sem_wait(sem_any);
+
+    if (sem_trywait(sem_full_priority) == 0) {
+      queue_name = "PRIORITY";
+      sem_wait(sem_mutex_priority);
+      strcpy(str, shdata->priority_buf[shdata->priority_out]);
+      shdata->priority_out =
+          (shdata->priority_out + 1) % ((K / 2) / (STR_LEN + 1));
+      sem_post(sem_mutex_priority);
+      sem_post(sem_empty_priority);
+    } else {
+      queue_name = "NORMAL";
+      sem_wait(sem_full_normal);
+      sem_wait(sem_mutex_normal);
+      strcpy(str, shdata->normal_buf[shdata->normal_out]);
+      shdata->normal_out = (shdata->normal_out + 1) % ((K / 2) / (STR_LEN + 1));
+      sem_post(sem_mutex_normal);
+      sem_post(sem_empty_normal);
+    }
+
+    int pos = snprintf(line, sizeof line, "[Consumer %d] %s read: ", id + 1,
+                       queue_name);
+    write(STDOUT_FILENO, line, pos);
+    for (int i = 0; str[i] != '\0'; i++) {
+      write(STDOUT_FILENO, &str[i], 1);
+      usleep(300000);
+    }
+    write(STDOUT_FILENO, "\n", 1);
+  }
+}
+
+void manager(void) {
+  char str[STR_LEN + 1];
+  while (1) {
+    sleep(MANAGER_INTERVAL);
+
+    int np, nn;
+    sem_getvalue(sem_full_priority, &np);
+    sem_getvalue(sem_full_normal, &nn);
+    printf("[Manager] stats: priority=%d normal=%d\n", np, nn);
+    fflush(stdout);
+
+    if (sem_trywait(sem_full_normal) != 0)
+      continue;
+
+    sem_wait(sem_empty_priority);
+
+    sem_wait(sem_mutex_normal);
+    strcpy(str, shdata->normal_buf[shdata->normal_out]);
+    shdata->normal_out = (shdata->normal_out + 1) % ((K / 2) / (STR_LEN + 1));
+    sem_post(sem_mutex_normal);
+    sem_post(sem_empty_normal);
+
+    sem_wait(sem_mutex_priority);
+    strcpy(shdata->priority_buf[shdata->priority_in], str);
+    shdata->priority_in = (shdata->priority_in + 1) % ((K / 2) / (STR_LEN + 1));
+    sem_post(sem_mutex_priority);
+    sem_post(sem_full_priority);
+
+    printf("[Manager] promoted NORMAL -> PRIORITY: %s\n", str);
+    fflush(stdout);
+  }
+}
+
+int main(void) {
+  char name[64];
+  int shm_fd;
+
+  shm_unlink(SHM_NAME);
+  sem_unlink(SEM_ANY);
+  sem_unlink(SEM_FULL_PRIORITY);
+  sem_unlink(SEM_EMPTY_PRIORITY);
+  sem_unlink(SEM_MUTEX_PRIORITY);
+  sem_unlink(SEM_FULL_NORMAL);
+  sem_unlink(SEM_EMPTY_NORMAL);
+  sem_unlink(SEM_MUTEX_NORMAL);
+  for (int i = 0; i < N; i++) {
+    snprintf(name, sizeof name, SEM_ORDER, i);
+    sem_unlink(name);
+  }
+
+  if ((shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666)) == -1) {
+    perror("shm_open");
+    exit(1);
+  }
+  ftruncate(shm_fd, sizeof(SharedData));
+  if ((shdata = mmap(NULL, sizeof(SharedData), PROT_READ | PROT_WRITE,
+                     MAP_SHARED, shm_fd, 0)) == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+  close(shm_fd);
+
+  if ((sem_any = sem_open(SEM_ANY, O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED) {
+    perror("any");
+    exit(1);
+  }
+  if ((sem_full_priority = sem_open(SEM_FULL_PRIORITY, O_CREAT | O_EXCL, 0666,
+                                    0)) == SEM_FAILED) {
+    perror("full_priority");
+    exit(1);
+  }
+  if ((sem_empty_priority = sem_open(SEM_EMPTY_PRIORITY, O_CREAT | O_EXCL, 0666,
+                                     (K / 2) / (STR_LEN + 1))) == SEM_FAILED) {
+    perror("empty_priority");
+    exit(1);
+  }
+  if ((sem_mutex_priority = sem_open(SEM_MUTEX_PRIORITY, O_CREAT | O_EXCL, 0666,
+                                     1)) == SEM_FAILED) {
+    perror("mutex_priority");
+    exit(1);
+  }
+  if ((sem_full_normal = sem_open(SEM_FULL_NORMAL, O_CREAT | O_EXCL, 0666,
+                                  0)) == SEM_FAILED) {
+    perror("full_normal");
+    exit(1);
+  }
+  if ((sem_empty_normal = sem_open(SEM_EMPTY_NORMAL, O_CREAT | O_EXCL, 0666,
+                                   (K / 2) / (STR_LEN + 1))) == SEM_FAILED) {
+    perror("empty_normal");
+    exit(1);
+  }
+  if ((sem_mutex_normal = sem_open(SEM_MUTEX_NORMAL, O_CREAT | O_EXCL, 0666,
+                                   1)) == SEM_FAILED) {
+    perror("mutex_normal");
+    exit(1);
+  }
+  for (int i = 0; i < N; i++) {
+    snprintf(name, sizeof name, SEM_ORDER, i);
+    if ((sem_order[i] = sem_open(name, O_CREAT | O_EXCL, 0666,
+                                 i == 0 ? 1 : 0)) == SEM_FAILED) {
+      perror(name);
+      exit(1);
+    }
+  }
+
+  printf("Producers: %d  Consumers: %d  Buffer: %d  Priority: %d%%  "
+         "Manager every %ds\n\n",
+         N, M, K, PRIORITY_CHANCE, MANAGER_INTERVAL);
+
+  pid_t pid;
+  for (int i = 0; i < N; i++) {
+    if ((pid = fork()) == -1) {
+      perror("fork producer");
+      exit(1);
+    }
+    if (pid == 0) {
+      producer(i);
+      exit(1);
+    }
+  }
+  for (int i = 0; i < M; i++) {
+    if ((pid = fork()) == -1) {
+      perror("fork consumer");
+      exit(1);
+    }
+    if (pid == 0) {
+      consumer(i);
+      exit(1);
+    }
+  }
+
+  if ((pid = fork()) == -1) {
+    perror("fork manager");
+    exit(1);
+  }
+  if (pid == 0) {
+    manager();
+    exit(1);
+  }
+
+  for (int i = 0; i < N + M + 1; i++)
+    wait(NULL);
+
+  shm_unlink(SHM_NAME);
+  sem_unlink(SEM_ANY);
+  sem_unlink(SEM_FULL_PRIORITY);
+  sem_unlink(SEM_EMPTY_PRIORITY);
+  sem_unlink(SEM_MUTEX_PRIORITY);
+  sem_unlink(SEM_FULL_NORMAL);
+  sem_unlink(SEM_EMPTY_NORMAL);
+  sem_unlink(SEM_MUTEX_NORMAL);
+  for (int i = 0; i < N; i++) {
+    snprintf(name, sizeof name, SEM_ORDER, i);
+    sem_unlink(name);
+  }
+
+  return 0;
+}
